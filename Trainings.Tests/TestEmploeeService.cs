4a11805d53cb3b0084ded235a75794397d3220dc
Nikulin Dmitriy <dmin@ciklum.com>
2015-10-28 17:37:35 +0300

    Added unit tests

---
+++ Trainings.Tests/TestEmploeeService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+using Trainings.Entities;
+using Trainings.Services;
+
+namespace Trainings.Tests
+{
+	[TestFixture]
+	public class TestEmploeeService
+	{
+		[Test]
+		public void Test_GetLatestTrainings_Have2EmploeesAndOneHasResentDate_FoundOneEmploee()
+		{
+			// Arrange 
+			var testEmploee = new Employee
+			{
+				EmployeeTrainings = new List<EmployeeTraining>()
+				{
+					new EmployeeTraining()
+					{
+						TrainingVisits = new List<TrainingVisit>()
+						{
+							new TrainingVisit
+							{
+								Date = DateTime.Now.AddDays(-3),
+							}
+						}
+					},
+
+					new EmployeeTraining()
+					{
+						TrainingVisits = new List<TrainingVisit>()
+						{
+							new TrainingVisit
+							{
+								Date = DateTime.Now.AddDays(-20),
+							}
+						}
+					},
+				}
+			};
+
+			var serviceUnderTest = new EmploeeService(null);
+
+
+			// Act 
+			var actualVisits = serviceUnderTest.GetLatestTrainings(testEmploee);
+
+			// Assert
+			Assert.AreEqual(1, actualVisits.Count);
+
+			const int recentDays = 10;
+			var maxDate = DateTime.Now.AddDays(-recentDays);
+			Assert.True(actualVisits.First().Date > maxDate);
+		}
+	}
+}
