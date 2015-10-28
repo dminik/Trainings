b1aeaeea3965cdf8f4c8cc874c287c1d553d94a3
Nikulin Dmitriy <dmin@ciklum.com>
2015-10-28 15:32:10 +0300

    Added empty EmployeeRepository

---
+++ Trainings/Repositories/EmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+using Trainings.Entities;
+
+namespace Trainings.Repositories
+{
+	public class EmployeeRepository
+	{
+		public bool Insert(Employee employee)
+		{			
+			return false;
+		}
+
+		public bool Update(Employee employee)
+		{
+			return false;
+		}
+	}
+}
---
+++ Trainings/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿using Trainings.Entities;
+
+namespace Trainings.Repositories
+{
+	public interface IEmployeeRepository
+	{		
+		bool Insert(Employee employee);
+
+		bool Update(Employee employee);	
+	}
+}
